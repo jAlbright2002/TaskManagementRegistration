5633079fd90354ad18de22af73fc37641f5d7466
james <G00379271@atu.ie>
2024-11-10 04:10:42 +0000

    Password is now encoded before storage in db.
    Added SecurityConfig for Spring Security configuration.
    
    The config is basic but is mainly for use of BCryptPasswordEncoder.
    
    CRSF is disabled and all requests are allowed

--- src/main/java/ie/atu/taskmanagementregistration/Authentication/AuthController.java
+++ src/main/java/ie/atu/taskmanagementregistration/Authentication/AuthController.java
@@ -3,6 +3,7 @@ package ie.atu.taskmanagementregistration.Authentication;
 import ie.atu.taskmanagementregistration.User.User;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final BCryptPasswordEncoder passEncoder;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, BCryptPasswordEncoder passEncoder) {
         this.authService = authService;
+        this.passEncoder = passEncoder;
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@Valid @RequestBody User user) {
+        user.setPassword(passEncoder.encode(user.getPassword()));
         return authService.register(user);
     }
 
