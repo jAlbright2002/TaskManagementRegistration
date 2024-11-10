0737df796b65751511e4ff05b53314e15ecfcb23
james <G00379271@atu.ie>
2024-11-10 02:05:25 +0000

    Added validation for user properties

--- src/main/java/ie/atu/taskmanagementregistration/User/User.java
+++ src/main/java/ie/atu/taskmanagementregistration/User/User.java
@@ -1,5 +1,8 @@
 package ie.atu.taskmanagementregistration.User;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,12 +14,21 @@ public class User {
     @Id
     String id;
 
+    @Email(message = "Email invalid, must be structured as 'youremail@domain'")
+    @NotEmpty(message = "Email must not be empty")
     String email;
 
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$",
+             message = "Password must have at least 8 characters, a symbol, one capital and small letter and one number")
+    @NotEmpty(message = "Password must not be empty")
     String password;
 
+    @NotEmpty(message = "Name must not be empty")
+    @Pattern(regexp = "^*[A-z].+", message = "Name must be longer than one character")
     String firstName;
 
+    @Pattern(regexp = "^(Student|Professional|Personal|Other)$",
+             message = "Category must be one of Student, Professional, Personal, Other")
     String userCategory;
 
 }
